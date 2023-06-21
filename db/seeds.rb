3b41ab889d700694781dd8c4dd04958a08190793
Megan Byrne <megsgetsemail@gmail.com>
2023-06-21 09:38:36 -0400

    Adds basic trips model with seeds with endpoint
    This namespaces routes under api/v1 for conventional API versioning practices. It also sets up the development rails env on port 3001 with a future CORS permission for 3000

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  start_date = Date.today + [*2..52].sample.weeks
+  end_date = start_date + [*3..21].sample.days
+
+  Trip.create(title: "#{Faker::Address.country} Trip!",
+              description: Faker::Lorem.paragraph,
+              start_date: start_date,
+              end_date: end_date)
+end
---
+++ spec/factories/trips.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :trip do
+    title { "MyString" }
+    description { "MyText" }
+    start_date { "2023-06-21" }
+    end_date { "2023-06-21" }
+  end
+end
