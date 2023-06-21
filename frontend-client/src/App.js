7409c44ca4152db74365ad05709acdb3c9b8fe2b
Megan Byrne <megsgetsemail@gmail.com>
2023-06-21 10:49:31 -0400

    Adds react frontend with basic trips component

---
+++ frontend-client/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import axios from "axios";
+import Trips from "./components/trips";
+import { useEffect, useState } from 'react';
+
+const API_URL = "http://localhost:3001/api/v1/trips";
+
+function getAPIData() {
+  return axios.get(API_URL).then((response) => response.data);
+}
+
+function App() {
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    getAPIData().then((items) => {
+      if (mounted) {
+        setTrips(items);
+      }
+    });
+    return () => (mounted = false);
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Your Trips</h1>
+      <Trips trips={trips} />
+    </div>
+  );
+}
+
+export default App;
