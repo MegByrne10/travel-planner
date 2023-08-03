2d3a8f2ed98f180f7d48813d9fc52473a8a1b3de
Megan Byrne <megsgetsemail@gmail.com>
2023-08-03 11:48:32 -0400

    Adds expandable panels and dropdown comps

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,8 +1,37 @@
+import { useState } from "react";
 import { GoBell, GoBeaker } from "react-icons/go";
 import Button from "./components/ui/Button";
 import FloatingLabelInput from "./components/ui/FloatingLabelInput";
+import Panel from "./components/ui/Panel";
+import Dropdown from "./components/ui/Dropdown";
+import ExpandablePanel from "./components/ui/ExpandablePanel";
 
 function App() {
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
+  const options = [
+    {label: 'Red', value: 'red' },
+    {label: 'Blue', value: 'blue' },
+    {label: 'Green', value: 'green' },
+  ]
+
+  const expandableHeader = <>
+    <Button accent small className="mr-3">Some Action</Button>
+    The Name of the Header
+  </>
+  const headerOne = <>
+    <Button secondary small className="mr-3">Another Action</Button>
+    Nested Header 1
+  </>
+  const headerTwo = <>
+    <Button secondary small className="mr-3">Another Action</Button>
+    Nested Header 2
+  </>
+
   return (
     <div className="container justify-center mx-auto px-4">
       <h1 className="text-3xl font-bold underline">
@@ -64,6 +93,33 @@ function App() {
               </div>
             </form>
           </div>
+        </div>
+      </div>
+      <div>
+        <h1 className="mt-5 text-3xl front-bold underline">Expandable Panels</h1>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <ExpandablePanel header={expandableHeader}>
+            <p>
+            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.
+            </p>
+            <ExpandablePanel header={headerOne}>
+              <p>
+              Etiam fermentum eleifend elit tristique tempor. Aenean eu nisl ultrices, vulputate justo quis, bibendum enim. Sed at massa feugiat eros suscipit aliquam et nec leo. Nulla varius sapien eu mattis feugiat. Etiam ut dignissim velit. Praesent a est a eros sagittis commodo sit amet vel nulla. Nam sodales nunc id tempor elementum. Suspendisse nec semper arcu, vitae lacinia urna. Duis nec hendrerit nisi. Phasellus rhoncus mauris mi, sit amet volutpat dui suscipit non. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla facilisis, nunc ac gravida tincidunt, diam justo egestas mauris, ac commodo metus quam non felis. Fusce vehicula bibendum sapien et suscipit.
+              </p>
+            </ExpandablePanel>
+            <ExpandablePanel header={headerTwo}>
+              <p>
+              Aliquam feugiat nec tortor ut aliquam. Vestibulum ac turpis eget arcu molestie elementum. Aenean dictum porta odio vel euismod. Etiam non vehicula justo, ut interdum mi. Integer pharetra eleifend mi, id faucibus leo tincidunt et. In velit nunc, feugiat at ultrices vitae, porta facilisis ex. Fusce congue congue nisi non posuere. Aenean imperdiet id ex viverra tristique. Phasellus elementum mollis urna nec blandit. Integer placerat pretium mi vitae ullamcorper.
+              </p>
+            </ExpandablePanel>
+          </ExpandablePanel>
+        </div>
+      </div>
+
+      <div>
+        <h1 className="mt-5 text-3xl front-bold underline">Dropdown</h1>
+        <div className="flex">
+          <Dropdown options={options} value={selection} onChange={handleSelect} />
         </div>
       </div>
     </div>
